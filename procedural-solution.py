23cfe909533237d3b0df557807e2ffe06f8d7881
= <petitalx@gmail.com>
2018-11-22 18:20:35 +0100

    find strongest beer

--- procedural-solution.py
+++ procedural-solution.py
@@ -1,1 +1,22 @@
 #!/usr/bin/env python3
+import json
+
+def strongest(datas):
+  max = 0
+  max_b = {}
+  for beer in datas:
+    cur = beer['Tx_Alcool']
+    if cur and cur > max:
+      max = cur
+      max_b = beer
+  print(max, max_b)
+
+def main():
+  with open('beer_list.json', 'r') as json_file:
+    datas = list(json.load(json_file))
+    strongest(datas)
+
+
+# sort by gout
+
+main()
